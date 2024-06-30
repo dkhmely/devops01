1a6e8906df00757b9089d38423fda0f2f44016d6
“Denys” <denys.khmelnytskyi@nthrive.com>
2024-06-30 20:37:23 +0200

    init

---
+++ backend/config/config.js
@@ -0,0 +1,11 @@
+/** @type {import('sequelize').Options} */
+module.exports = {
+  development: {
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_NAME,
+    host: process.env.DEV_DB_HOSTNAME,
+    dialect: process.env.DEV_DB_DIALECT,
+    logging: process.env.DEV_DB_LOGGING,
+  }
+};
